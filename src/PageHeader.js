c50ee68a86d0b7bc4867d72d1e893e9dabec5d42
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-08-27 16:41:11 -0700

    add linter and header component

---
+++ src/PageHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PageHeader.css';
+
+function PageHeader() {
+  return (
+    <div className="App">
+      <h1>Friend Zone</h1>
+    </div>
+  )
+}
+
+export default PageHeader
