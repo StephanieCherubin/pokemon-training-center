fe2d936f809ce5873abc08eec6ffd9e32de2281e
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-08-27 16:56:57 -0700

    add content to container

---
+++ src/PageContent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Project from './Project';
+
+function PageContent() {
+  return(
+    <div>
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+    </div>
+  )
+}
+
+export default PageContent
---
+++ src/Project.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Project() {
+  return(
+    <div>
+      <img src="#" width="300" height="200" alt="" />
+      <h3> Title of Project</h3>
+      <a href="#">Link to a Project</a>
+    </div>
+  )
+}
+
+export default Project;
