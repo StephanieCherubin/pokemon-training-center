8964e7ea4892ec52a89fa806839a7c2f3a5efec4
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-09-05 13:32:08 -0700

    add Footer component

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return(
+    <div className="Footer">
+      <h4>Github | LinkedIn</h4>
+    </div>
+  )
+}
+
+export default Footer;
